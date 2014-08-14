c6f7bc6e2df0d4d7b89c99da07fcd0e4fc36a19b
Tim Whitacre <tim@argyllstudios.com>
2014-08-14 13:11:05 -0400

    Initial Commit

---
+++ The Iron Yard/The Forge/Demo/main.js
@@ -0,0 +1,124 @@
+requirejs.config({
+  baseUrl: "http://cdn.wijmo.com/amd-js/3.20142.45",
+    paths: {
+      "jquery": "jquery-1.11.1.min",
+      "jquery-ui": "jquery-ui-1.11.0.custom.min",
+      "jquery.ui": "jquery-ui",
+      "jquery.mousewheel": "jquery.mousewheel.min",
+      "globalize": "globalize.min"
+    }
+});
+
+require(["wijmo.wijgrid", "wijmo.wijpiechart"], function () {
+    $(document).ready(function () {
+        $("#wijgrid").wijgrid({
+            allowSorting: true,
+            allowPaging: true,
+            pageSize: 10,
+            data: pdata,
+            columns: [
+                {headerText: "Last Name"},
+                {headerText: "First Name"},
+                {headerText: "MRN"},
+                {headerText: "Age"},
+								{headerText: "Doctor"},
+								{headerText: "Hospital"},
+								{headerText: "Room"},
+								{headerText: "Flag"}
+            ]
+        });
+    });
+
+
+
+	$(document).ready(function () {
+	    $("#wijpiechart").wijpiechart({
+        radius: 140,
+        hint: {
+            content: function () {
+                return this.data.label + " : " + Globalize.format(this.value / this.total, "p2");
+            }
+        },
+        seriesList: [{
+            label: "Dr. Cushing",
+            data: 5,
+            offset: 0
+        }, {
+            label: "Dr. Dandy",
+            data: 1,
+            offset: 0
+        }, {
+            label: "Dr. Osler",
+            data: 4,
+            offset: 0
+        }],
+        seriesStyles: [{
+            fill: "180-rgb(195,255,0)-rgb(175,229,0)",
+            stroke: "rgb(175,229,0)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(142,222,67)-rgb(127,199,60)",
+            stroke: "rgb(127,199,60)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(106,171,167)-rgb(95,153,150)",
+            stroke: "rgb(95,153,150)",
+            "stroke-width": 1.5
+        }]
+    });
+
+		$("#wijpiechartr").wijpiechart({
+			radius: 140,
+			hint: {
+					content: function () {
+							return this.data.label + " : " + Globalize.format(this.value / this.total, "p2");
+					}
+			},
+			seriesList: [{
+					label: "Age: 1-20",
+					data: 2,
+					offset: 0
+			}, {
+					label: "Age: 21-30",
+					data: 1,
+					offset: 0
+			}, {
+					label: "Age: 31-50",
+					data: 3,
+					offset: 0
+			},{
+				label: "Age: 51-80",
+				data: 3,
+				offset: 0
+			},{
+				label: "Age: 80+",
+				data: 1,
+				offset: 0
+			}],
+			seriesStyles: [{
+            fill: "180-rgb(195,255,0)-rgb(175,229,0)",
+            stroke: "rgb(175,229,0)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(142,222,67)-rgb(127,199,60)",
+            stroke: "rgb(127,199,60)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(106,171,167)-rgb(95,153,150)",
+            stroke: "rgb(95,153,150)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(70,106,133)-rgb(62,95,119)",
+            stroke: "rgb(62,95,119)",
+            "stroke-width": 1.5
+        }, {
+            fill: "90-rgb(166,166,166)-rgb(149,149,149)",
+            stroke: "rgb(149,149,149)",
+            "stroke-width": 1.5
+        }]
+	});
+
+
+	});
+
+});
---
+++ The Iron Yard/The Forge/presentation.md
@@ -0,0 +1,39 @@
+
+### Intro
+
+What is considered the "front end" of a website or web application?
+
+The front end is the part of the web that you can see and interact with. It usually consists of two parts, the design and the frontend development.
+
+Designers vs FE Developers vs BE evelopers
+
+HTML, CSS & JavaScript
+
+### HTML & CSS
+
+HTML is a 'markup language' and CSS is a 'stylesheet language'
+
+HTML is designed to store content & CSS is designed to customize how the content is displayed.
+
+HTML & CSS describes presentation
+
+### JavaScript
+
+JavaScript is an object-oriented language.
+
+JavaScript describes function.
+
+-----
+
+Take for instance Gmail. All of that interaction happens without the page ever refreshing. However as powerful as this is, we need to still talk to a backend in order to retrive our but then it's up to us as how we want to display it.
+
+-----
+
+Car Analogy
+
+HTML - The car frame
+CSS - The car design / features
+JavaScript - The engine that powers the car
+
+
+### Demo Time
